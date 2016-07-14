9fcbc51dfe567741d9161805eb95db92ee0ddd01
Adam J. Penn <madannep@gmail.com>
2016-07-14 14:45:05 -0700

    fixed injector error suck it sterling

--- jsdmp/public/javascripts/app.js
+++ jsdmp/public/javascripts/app.js
@@ -1,40 +1,40 @@
-var app = angular.module('jsdmp', ['AppCtrl']);
+var app = angular.module('jsdmp', []);
 app.factory('socket', function($rootScope) {
-    var socket;
-    socket = io.connect("http://localhost:8080");
-    return {
-      on: function(eventName, callback) {
-        return socket.on(eventName, function() {
-          var args;
-          args = arguments;
-          return $rootScope.$apply(function() {
-            return callback.apply(socket, args);
-          });
+  var socket;
+  socket = io.connect("http://localhost:8080");
+  return {
+    on: function(eventName, callback) {
+      return socket.on(eventName, function() {
+        var args;
+        args = arguments;
+        return $rootScope.$apply(function() {
+          return callback.apply(socket, args);
         });
-      },
-      emit: function(eventName, data, callback) {
-        return socket.emit(eventName, data, function() {
-          var args;
-          args = arguments;
-          return $rootScope.$apply(function() {
-            return callback.apply(socket, args);
-          });
+      });
+    },
+    emit: function(eventName, data, callback) {
+      return socket.emit(eventName, data, function() {
+        var args;
+        args = arguments;
+        return $rootScope.$apply(function() {
+          return callback.apply(socket, args);
         });
-      }
-    };
-  });
-app.controller('AppCtrl', ['$scope', 'socket', function($scope, socket) {
-    $scope.init = function() {
-        mySocket.emit('job:request', {});
-        mySocket.on('job:new_job', $scope.compute);
-    };
-    // Computes job, when done returns the result
-    $scope.compute = function(job) {
-        console.log('job', job)
-        var data = job.data;
-        var output = job.function(data);
-        mySocket.emit('job:completed', {
-            result: output
-        });
-    };
+      });
+    }
+  };
+});
+app.controller('AppCtrl', ['$scope', function($scope) {
+  $scope.init = function() {
+      mySocket.emit('job:request', {});
+      mySocket.on('job:new_job', $scope.compute);
+  };
+  // Computes job, when done returns the result
+  $scope.compute = function(job) {
+      console.log('job', job)
+      var data = job.data;
+      var output = job.function(data);
+      mySocket.emit('job:completed', {
+          result: output
+      });
+  };
 }]);
--- jsdmp/public/javascripts/demo.js
+++ jsdmp/public/javascripts/demo.js
@@ -1,10 +1,10 @@
-var app = angular.module('demo', ['btford.socket-io', 'demo.AppCtrl']);
-
-app.controller('DemoCtrl', function($scope) {
-  $scope.numberOfUsers;
-  mySocket.on('statusUpdate' function(data) {
-    $scope.activeUser = data.activeUser;
-    $scope.approximation = data.approximation;
-    $scope.jobsCompleted = data.jobsCompleted;
-  });
-});
+// var app = angular.module('demo', ['btford.socket-io', 'demo.AppCtrl']);
+//
+// app.controller('DemoCtrl', function($scope) {
+//   $scope.numberOfUsers;
+//   mySocket.on('statusUpdate' function(data) {
+//     $scope.activeUser = data.activeUser;
+//     $scope.approximation = data.approximation;
+//     $scope.jobsCompleted = data.jobsCompleted;
+//   });
+// });
