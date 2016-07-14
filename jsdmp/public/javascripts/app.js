dc28032d97e760f69a4118b9b1bbb8c7d4978e19
Anna <anna.maulle@gmail.com>
2016-07-14 12:55:15 -0700

    added compute fucntion

--- jsdmp/public/javascripts/app.js
+++ jsdmp/public/javascripts/app.js
@@ -8,4 +8,9 @@ app.controller('AppCtrl', function($scope, mySocket) {
   $scope.init = function() {
     mySocket.emit('Hello Event', { data: 'Hi Team' });
   };
+// Computes job, when done returns the result
+  $scope.compute = function(job){
+    var data = job.data;
+    return output = job.function(data);
+  };
 });
