a3d149d51762f2277310a19071a8e63ba1caa370
Sterling Baldwin <sterling16@mac.com>
2016-07-14 11:14:53 -0700

    Adds websocket logic

--- jsdmp/app.js
+++ jsdmp/app.js
@@ -10,6 +10,38 @@ var users = require('./routes/users');
 
 var app = express();
 
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    // process HTTP request. Since we're writing just WebSockets server
+    // we don't have to implement anything.
+});
+server.listen(1337, function() { });
+
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            // process WebSocket message
+        }
+    });
+
+    connection.on('close', function(connection) {
+        // close user connection
+    });
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
---
+++ jsdmp/public/javascripts/jsdmp.js
@@ -0,0 +1,26 @@
+$(function () {
+    // if user is running mozilla then use it's built-in WebSocket
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+    var connection = new WebSocket('ws://127.0.0.1:1337');
+
+    connection.onopen = function () {
+        console.log('connection ready');
+    };
+
+    connection.onerror = function (error) {
+        console.log('connection error');
+    };
+
+    connection.onmessage = function (message) {
+        // try to decode json (I assume that each message from server is json)
+        try {
+            var json = JSON.parse(message.data);
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+        console.log('got a message');
+        console.log(message);
+    };
+});
