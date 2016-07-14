044de621efa52f1d4717def10c73f69782faa870
Sterling Baldwin <sterling16@mac.com>
2016-07-14 12:07:59 -0700

    adds jsdmp.js and requires it form the app.js

--- jsdmp/app.js
+++ jsdmp/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
+var jsdmp = require('./jsdmp.js');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
