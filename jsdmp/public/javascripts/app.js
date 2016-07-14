376422fc64dd866ad7d37e5b0f2aeada80a5ccd3
Sterling Baldwin <sterling16@mac.com>
2016-07-14 14:55:30 -0700

    attempting to get the sockets communicating

--- jsdmp/public/javascripts/app.js
+++ jsdmp/public/javascripts/app.js
@@ -23,17 +23,17 @@ app.factory('socket', function($rootScope) {
     }
   };
 });
-app.controller('AppCtrl', ['$scope', function($scope) {
+app.controller('AppCtrl', ['$scope', 'socket', function($scope, socket) {
   $scope.init = function() {
-      mySocket.emit('job:request', {});
-      mySocket.on('job:new_job', $scope.compute);
+      socket.emit('job:request', {});
+      socket.on('job:new_job', $scope.compute);
   };
   // Computes job, when done returns the result
   $scope.compute = function(job) {
       console.log('job', job)
       var data = job.data;
       var output = job.function(data);
-      mySocket.emit('job:completed', {
+      socket.emit('job:completed', {
           result: output
       });
   };
