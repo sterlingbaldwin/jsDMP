2795f4e5f2ed679e5e216acadec69b7c5fef0723
Bryce Sampson <sampson.bryce@yahoo.com>
2016-07-14 16:47:50 -0700

    timing total

--- jsdmp/app.js
+++ jsdmp/app.js
@@ -18,44 +18,44 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 
-var generator = function(current, step, compute){
-  var job = {
-    data: {
-      start: this.current_position,
-      end: this.current_position + this.step_size,
-      step_size: this.step_size
-    },
-    compute_function: this.compute_function
-  };
-  this.current_position += this.step_size;
-  this.jobq.push(job);
+var generator = function(current, step, compute) {
+    var job = {
+        data: {
+            start: this.current_position,
+            end: this.current_position + this.step_size,
+            step_size: this.step_size
+        },
+        compute_function: this.compute_function
+    };
+    this.current_position += this.step_size;
+    this.jobq.push(job);
 }
-var aggregator = function(data){
-  return this.total += data.result;
+var aggregator = function(data) {
+    return this.total += data.result;
 }
 
-var compute_function = function(args){
-  var a = args.start;
-  var b = args.end;
-  var step_size = args.step_size;
-  var start = Math.cos(a/3) - Math.cos(a/5) + Math.sin(a/4) + 8;
-  var stop = Math.cos(b/3) - Math.cos(b/5) + Math.sin(b/4) + 8;
-  return ((start + stop)/2) * step_size;
+var compute_function = function(args) {
+    var a = args.start;
+    var b = args.end;
+    var step_size = args.step_size;
+    var start = Math.cos(a / 3) - Math.cos(a / 5) + Math.sin(a / 4) + 8;
+    var stop = Math.cos(b / 3) - Math.cos(b / 5) + Math.sin(b / 4) + 8;
+    return ((start + stop) / 2) * step_size;
 }
 
 compute_function = compute_function.toString();
-
+var start_time = null;
 var start = 100;
 var stop = 600;
-var step_size = (stop-start)/1000;
+var step_size = (stop - start) / 1000;
 jsdmp.init({
-  step_size: step_size,
-  generator: generator,
-  aggregator: aggregator,
-  compute_function: compute_function
+    step_size: step_size,
+    generator: generator,
+    aggregator: aggregator,
+    compute_function: compute_function
 });
-jsdmp.find_error = function(){
-  return (Math.abs(jsdmp.target - jsdmp.total)/jsdmp.total) * 100;
+jsdmp.find_error = function() {
+    return (Math.abs(jsdmp.target - jsdmp.total) / jsdmp.total) * 100;
 }
 jsdmp.current_position = start;
 jsdmp.start = start;
@@ -65,38 +65,42 @@ jsdmp.target = 4003.72090015132682659;
 jsdmp.complete = false;
 
 var io = require('socket.io')(8080);
-io.on('connection', function(client){
-  client.on('connect', function(data){
+io.on('connection', function(client) {
     console.log('********* Client connected *********');
-  });
-
-  client.on('disconnect', function(data){
-    console.log('client disconnected');
-  });
-
-  client.on('job:request', function(data){
-    if(jsdmp.complete == true){
-      return;
+    if(!start_time){
+        console.log('setting start time');
+        start_time = new Date()
     }
-    jsdmp.generator();
-    var job = jsdmp.jobq.pop();
-    if(!job.data.start){
-      jsdmp.generator();
-      job = jsdmp.jobq.pop();
-    }
-    client.emit('job:new_job', job);
-  });
+    
+    client.on('disconnect', function(data) {
+        console.log('client disconnected');
+    });
 
-  client.on('job:completed', function(data){
-    jsdmp.aggregator(data);
-    var error = jsdmp.find_error()
-    if(error < 0.05){
-      jsdmp.complete = true;
-      console.log('complete!');
-      console.log("current total: " + jsdmp.total);
-      console.log("current error: " + error);
-    }
-  })
+    client.on('job:request', function(data) {
+        if (jsdmp.complete == true) {
+            return;
+        }
+        jsdmp.generator();
+        var job = jsdmp.jobq.pop();
+        if (!job.data.start) {
+            jsdmp.generator();
+            job = jsdmp.jobq.pop();
+        }
+        client.emit('job:new_job', job);
+    });
+
+    client.on('job:completed', function(data) {
+        jsdmp.aggregator(data);
+        var error = jsdmp.find_error()
+        if (error < 0.05) {
+            jsdmp.complete = true;
+            console.log('complete!');
+            console.log("current total: " + jsdmp.total);
+            console.log("current error: " + error);
+            var end_time = new Date();
+            console.log('completion time:', (end_time.getTime() - start_time.getTime()) / 1000)
+        }
+    })
 });
 
 
@@ -110,7 +114,9 @@ io.on('connection', function(client){
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -119,9 +125,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -129,23 +135,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
