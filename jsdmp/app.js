423f91c1e5fd797923687f2db6387d1b61a94e9d
Sterling Baldwin <sterling16@mac.com>
2016-07-14 11:43:51 -0700

    trying to get handlebars working

--- jsdmp/app.js
+++ jsdmp/app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 var WebSocketServer = require('websocket').server;
 var http = require('http');
@@ -43,8 +47,8 @@ wsServer.on('request', function(request) {
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
